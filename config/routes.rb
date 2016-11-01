1b4f6037730d4866ff3acaee904adabbc186d47a
Ruth Lehane <rlehane@tcd.ie>
2016-11-01 00:05:06 +0000

    all colours changed to yellow and drop down gone

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'pages/contact'
   get 'pages/brands'
   get 'pages/knowledge'
+  get 'pages/products'
 
   get 'pages/about'
   # The priority is based upon order of creation: first created -> highest priority.
